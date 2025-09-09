45780eb92eee60dc624d7b390f2e2a4302685f9f
filipporaw <116605816+filipporaw@users.noreply.github.com>
2025-09-09 13:22:58 +0100

    Delete cv directory

--- cv/src/app/components/themes/constants.tsx
+++
@@ -1,8 +0,0 @@
-import { ResumePDFDefault } from "./default";
-import { ResumePDFMinimal } from "./minimal";
-import { ThemeComponent } from "./types";
-
-export const THEMES: Record<string, ThemeComponent> = {
-  "default": ResumePDFDefault,
-  "minimal": ResumePDFMinimal
-};
--- cv/src/app/components/themes/lib.ts
+++
@@ -1,9 +0,0 @@
-import { THEMES } from "./constants";
-
-export const getAllThemesAvailable = () => {
-  return Object.keys(THEMES);
-};
-
-export const getTheme = (theme: string) => {
-  return THEMES[theme];
-};
